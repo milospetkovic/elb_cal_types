7c9f03b77be68c0b712a0381c937e4ab2b06f391
milospetkovic <elitasoft.web@gmail.com>
2020-03-20 18:45:44 +0100

    set property of sidebar to open when user first time open calendar type

--- src/App.vue
+++ src/App.vue
@@ -142,7 +142,7 @@ export default {
 			updating: false,
 			isAdminUser: false,
             defaultCalReminders: [],
-			selectedOpenSidebar: false,
+			selectedOpenSidebar: true,
             assignedReminders: []
         }
     },
