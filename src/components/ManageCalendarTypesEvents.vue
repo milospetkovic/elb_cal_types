0d14965f3d48017a5710b4decfd3b7e67b2b5e4e
milospetkovic <elitasoft.web@gmail.com>
2020-04-07 10:52:58 +0200

    update

--- src/components/ManageCalendarTypesEvents.vue
+++ src/components/ManageCalendarTypesEvents.vue
@@ -1,67 +1,99 @@
 <template>
-    <div id="content" class="app-elb-cal-types-events">
-        <AppNavigation>
+	<div id="content" class="app-elb-cal-types-events">
+		<AppNavigation>
+			<div class="nav-title-field">
+				<h4>{{ t('elb_cal_types', 'Assigned calendar types') }}</h4>
+			</div>
 
-            <div class="nav-title-field">
-                <h4>{{ t('elb_cal_types', 'Assigned calendar types') }}</h4>
-            </div>
+			<AppNavigationNew
+				:text="t('elbcaltypes', 'Create event')"
+				:disabled="disableCreateNewEventButton"
+				button-id="new-caltype-event-button"
+				button-class="icon-add"
+				:title="t('elbcaltypes', 'Create and assign calendar event with reminders to users')"
+				@click="newCalendarTypeEvent" />
 
-            <AppNavigationNew
-                    :text="t('elbcaltypes', 'Create event')"
-                    :disabled="disableCreateNewEventButton"
-                    button-id="new-caltype-event-button"
-                    button-class="icon-add"
-                    :title="t('elbcaltypes', 'Create and assign calendar event with reminders to users')"
-                    @click="newCalendarTypeEvent" />
+			<ul>
+				<template v-for="calType in assignedCalendarTypes">
+					<AppNavigationItem :key="calType.id" :item="calTypeEntry(calType)" icon="icon-user" />
+				</template>
+			</ul>
+		</AppNavigation>
 
-            <ul>
-                <template v-for="calType in assignedCalendarTypes">
-                    <AppNavigationItem :key="calType.id" :item="calTypeEntry(calType)" icon="icon-user">
-                    </AppNavigationItem>
-                </template>
-            </ul>
+		<AppContent>
+			<div v-if="currentCalTypeLinkID">
+				<h2>{{ t('elb_cal_types', 'Manage events for selected calendar type') }}: {{ assignedCalendarTypes[currentCalTypeLinkID]['cal_type_title'] }}</h2>
+			</div>
+			<div v-else>
+				<h2>{{ t('elb_cal_types', 'Please choose calendar type from the left menu to manage events') }}</h2>
+			</div>
 
-        </AppNavigation>
-
-        <AppContent>
-
-            <div v-if="currentCalTypeLinkID">
-                <h2>{{ t('elb_cal_types', 'Manage events for selected calendar type') }}: {{ assignedCalendarTypes[currentCalTypeLinkID]['cal_type_title'] }}</h2>
-            </div>
-            <div v-else>
-                <h2>{{ t('elb_cal_types', 'Please choose assigned calendar type from the left menu to manage events') }}</h2>
-            </div>
-
-        </AppContent>
-    </div>
+			<div v-if="visibleCreateNewEventForm">
+				<div class="table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th></th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>{{ t('elb_cal_types', 'Event name') }}</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>{{ t('elb_cal_types', 'Reminders for event') }}</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>{{ t('elb_cal_types', 'Assign users for event') }}</td>
+								<td></td>
+							</tr>
+						</tbody>
+						<tfoot>
+							<tr>
+								<td />
+								<td>
+									<button @click="cancelCreateNewEvent">
+										{{ t('elb_cal_types', 'Cancel') }}
+									</button>
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</AppContent>
+	</div>
 </template>
 
 <script>
 import {
-    AppNavigation,
-    AppNavigationNew,
-    AppNavigationItem,
-    AppContent,
+	AppNavigation,
+	AppNavigationNew,
+	AppNavigationItem,
+	AppContent,
 } from 'nextcloud-vue'
 
 import axios from '@nextcloud/axios'
 
 export default {
-    name: 'ManageCalendarTypesEvents',
-    components: {
-        AppNavigation,
+	name: 'ManageCalendarTypesEvents',
+	components: {
+		AppNavigation,
 		AppNavigationNew,
 		AppNavigationItem,
 		AppContent,
-    },
-    data() {
-    	return {
-    		assignedCalendarTypes: [],
+	},
+	data() {
+		return {
+			assignedCalendarTypes: [],
 			currentCalTypeLinkID: null,
-            visibleCreateNewEventForm: false,
-        }
-    },
-    computed: {
+			visibleCreateNewEventForm: false,
+		}
+	},
+	computed: {
 		/**
 		 * Return the item object for the sidebar entry of a calendar type
 		 * @returns {Object}
@@ -78,38 +110,41 @@ export default {
 		disableCreateNewEventButton() {
 			if (!this.currentCalTypeLinkID || this.visibleCreateNewEventForm) {
 				return true
-            }
+			}
 			return false
-        },
-    },
-    beforeMount() {
-    	axios.get(OC.generateUrl('/apps/elb_cal_types/getassignedcalendartypes')).then((result) => {
+		},
+	},
+	beforeMount() {
+		axios.get(OC.generateUrl('/apps/elb_cal_types/getassignedcalendartypes')).then((result) => {
 			this.assignedCalendarTypes = result.data
-			console.log('assigned cal types: ', this.assignedCalendarTypes)
+			// console.log('assigned cal types: ', this.assignedCalendarTypes)
 		})
 	},
-    methods: {
+	methods: {
 		openCalType(calType) {
 			this.visibleCreateNewEventForm = false
-            this.currentCalTypeLinkID = calType.link_id
+			this.currentCalTypeLinkID = calType.link_id
 		},
 		newCalendarTypeEvent() {
 			this.visibleCreateNewEventForm = true
-			//alert('implement event create form')
-        }
-    }
+			// alert('implement event create form')
+		},
+		cancelCreateNewEvent() {
+			this.visibleCreateNewEventForm = false
+		},
+	},
 }
 </script>
 <style scoped>
-    #app-content {
-        padding: 20px;
-    }
-    .nav-title-field {
-        display: inline-block;
-        padding: 10px 10px 10px 20px;
-        background-color: #EDEDED;
-        border: 1px solid #DBDBDB;
-        margin: 10px 10px;
-        font-weight: bold;
-    }
+	#app-content {
+		padding: 20px;
+	}
+	.nav-title-field {
+		display: inline-block;
+		padding: 10px 10px 10px 20px;
+		background-color: #EDEDED;
+		border: 1px solid #DBDBDB;
+		margin: 10px 10px;
+		font-weight: bold;
+	}
 </style>
