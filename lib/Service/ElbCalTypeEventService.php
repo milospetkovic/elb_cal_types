3428283f458b95b7213eeaebd140caf189e5a1b3
milospetkovic <elitasoft.web@gmail.com>
2020-04-09 12:30:43 +0200

    implemented first version - save form in cal_type_events table

--- lib/Service/ElbCalTypeEventService.php
+++ lib/Service/ElbCalTypeEventService.php
@@ -4,6 +4,7 @@
 namespace OCA\ElbCalTypes\Service;
 
 
+use OCA\Activity\CurrentUser;
 use OCA\ElbCalTypes\Db\CalendarTypeEvent;
 use OCA\ElbCalTypes\Db\CalendarTypeEventMapper;
 
@@ -17,14 +18,19 @@ class ElbCalTypeEventService
      * @var CalendarTypeEvent
      */
     private $calendarTypeEvent;
+    /**
+     * @var CurrentUser
+     */
+    private $currentUser;
 
     public function __construct(CalendarTypeEventMapper $calendarTypeEventMapper,
-                                CalendarTypeEvent $calendarTypeEvent)
+                                CalendarTypeEvent $calendarTypeEvent,
+                                CurrentUser $currentUser)
     {
         $this->calendarTypeEventMapper = $calendarTypeEventMapper;
         $this->calendarTypeEvent = $calendarTypeEvent;
+        $this->currentUser = $currentUser;
     }
-
 
     public function storeCalendarTypeEvent($data)
     {
@@ -32,8 +38,27 @@ class ElbCalTypeEventService
         $eventName = $data['eventname'];
         $eventDescription = $data['eventdesc'];
         $eventDateTime = $data['eventdatetime'];
+
+        // @TODO implement linking with those 2 related tables!!!
         $eventAssignedReminders = $data['reminders'];
         $eventAssignedUsers = $data['assigneduser'];
+
+        $calTypeEvent = new $this->calendarTypeEvent;
+        $calTypeEvent->setFkElbCalType($calTypeId);
+        $calTypeEvent->setUserAuthor($this->currentUser->getUID());
+        $calTypeEvent->setCreatedAt(date('Y-m-d H:i:s', time()));
+        $calTypeEvent->setTitle($eventName);
+        $calTypeEvent->setDescription($eventDescription);
+        $calTypeEvent->setEventDatetime($eventDateTime);
+        return $this->calendarTypeEventMapper->insert($calTypeEvent);
+
+
+//        $calTypeReminder = new CalendarTypeReminder();
+//        $calTypeReminder->setFkElbCalType($calendarTypeID);
+//        $calTypeReminder->setFkElbDefReminder($defReminderID);
+//        $calTypeReminder->setUserAuthor($this->currentUser->getUID());
+//        $calTypeReminder->setCreatedAt(date('Y-m-d H:i:s', time()));
+//        $this->mapper->insert($calTypeReminder);
 
         var_dump($data);
         die('stop');
